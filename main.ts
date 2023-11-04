7b58affa9dd751bead994965ee6dccb8e38a988f
Teo Zhi Hao <teozhihao99@gmail.com>
2023-11-04 16:40:08 +0800

    Initial commit

---
+++ main.ts
@@ -0,0 +1,61 @@
+// import packages
+import express, { response } from 'express'
+import morgan from 'morgan'
+import { engine } from 'express-handlebars'
+import { v4 as uuidv4 } from 'uuid'
+import { EventSource } from 'express-ts-sse'
+
+// default to 300 if PORT env is not set
+const port = process.env.PORT || 3000;
+
+// Create an instance of SSE
+const sse = new EventSource()
+
+// create an instance of the application
+const app = express()
+
+// Configure render
+app.engine('html', engine({ defaultLayout: false }))
+app.set('view engine', 'html')
+
+// Log incoming requests
+app.use(morgan('combined'))
+
+// POST /chess
+app.post('/chess', express.urlencoded({ extended: true }), (req, res) => {
+    const gameId = uuidv4().substring(0, 8)
+    const orientation = 'white'
+
+    res.status(200).render('chess', { gameId, orientation })
+})
+
+// GET /chess?gameId=abc123
+app.get('/chess', (req, res) => {
+    const gameId = req.query.gameId
+    const orientation = 'black'
+
+    res.status(200).render('chess', { gameId, orientation })
+})
+
+// PATCH /chess/:gameId
+app.patch('/chess/:gameId', express.json(), (req, res) => {
+    // Get gameId from the resource
+    const gameId = req.params.gameId
+    const move = req.body
+
+    console.log(`GameId: ${gameId}: `, move)
+    sse.send({ event: gameId, data: move })
+
+    res.status(201).json({ timestamp: (new Date()).getTime() })
+})
+
+// GET /chess/stream
+app.get('/chess/stream', sse.init)
+
+// Serve files from static
+app.use(express.static(__dirname + '/static'))
+
+// Start express
+app.listen(port, () => {
+    console.log(`Application bound to port ${port} at ${new Date().toLocaleString()}`)
+})
